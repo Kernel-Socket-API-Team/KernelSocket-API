573a3bfe721646505836556d180f4219157c9ca8
Igor <igorifoop12735490@gmail.com>
2026-04-30 11:11:17 +0300

    refactor(windows-server): The Windows context has been fixed. Redundant socket and active_client states have been removed. <TESTED>

--- src/common/windows/windows_common.h
+++ src/common/windows/windows_common.h
@@ -4,15 +4,6 @@
 #include "../../adapters/windows/windows_adapter.h"
 #include "../common.h"
 
-typedef enum
-{
-    SOCK_STATE_INIT = 0,      // Только создан
-    SOCK_STATE_BOUND = 1,     // Привязан к адресу
-    SOCK_STATE_LISTENING = 2, // TCP в режиме прослушивания
-    SOCK_STATE_CONNECTED = 3, // TCP подключен (клиент или принятый)
-    SOCK_STATE_UDP = 4,       // UDP сокет
-} net_socket_state_t;
-
 // Глобальный контекст WSK
 typedef struct WSK_CONTEXT
 {
@@ -38,11 +29,10 @@ net_error_t convert_status_from_windows(NTSTATUS ntstatus);
  */
 NTSTATUS normalize_ipv6_scope_id(const char* str, char* output, SIZE_T output_size);
 
-// Контекст сокета (упрощенный)
+// Контекст сокета
 typedef struct WINDOWS_SOCKET_IMPL
 {
     PWSK_SOCKET wsk_socket;    // WSK сокет
-    PWSK_SOCKET active_client; // Для TCP клиента
     KEVENT completion_event;   // Для синхронизации
 } WINDOWS_SOCKET_IMPL, *PWINDOWS_SOCKET_IMPL;
 
